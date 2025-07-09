e8fc161adfa8cf4c8eb7ef22a461be231219a1e5
inacio <eusouoinacio@gmail.com>
2025-07-09 14:14:42 -0300

    components createds with styles

--- blog/src/app/app.ts
+++ blog/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitle } from "./components/menu-title/menu-title";
+import { Home } from "./pages/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuTitle, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'projeto-blog';
+  protected title = 'blog';
 }
